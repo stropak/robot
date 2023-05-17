99f1d863cbd1996cb712767b443fbcd937b3105c
stropak <64900331+stropak@users.noreply.github.com>
2023-05-17 19:35:48 +0200

    Update motors.py

--- motors.py
+++ motors.py
@@ -6,11 +6,11 @@ import RPi.GPIO as GPIO
 class Motors:
 
     def __init__(self) -> None:
-        # nastaveni frekvence pwm
+        # set frequency
         self.PWM_FREQUENCY = 50
         self.SPEED = 90
 
-        # označení pinů
+        # label pins
         self.MR_ACTIVATE = 26
         self.ML_ACTIVATE = 12
         self.MR_F = 21
@@ -18,7 +18,7 @@ class Motors:
         self.ML_F = 13
         self.ML_B = 6
 
-        # Nastavení GPIO pinů jako vytsup
+        # set GPIO pins as output 
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         GPIO.setup(self.MR_ACTIVATE, GPIO.OUT)
@@ -28,90 +28,90 @@ class Motors:
         GPIO.setup(self.ML_F, GPIO.OUT)
         GPIO.setup(self.ML_B, GPIO.OUT)
 
-        # nastaveni pwm frekvence motoru
+        # set frequency pwm frekvency of motors
         self.power_MR = GPIO.PWM(self.MR_ACTIVATE, self.PWM_FREQUENCY)
         self.power_ML = GPIO.PWM(self.ML_ACTIVATE, self.PWM_FREQUENCY)
 
-    # jízda vpřed
+    # move forward
     def forward(self):
         GPIO.output(self.MR_F, 1)
         GPIO.output(self.MR_B, 0)
         GPIO.output(self.ML_F, 1)
         GPIO.output(self.ML_B, 0)
 
-    # jízda vzad
+    # move backward
     def backward(self):
         GPIO.output(self.MR_F, 0)
         GPIO.output(self.MR_B, 1)
         GPIO.output(self.ML_F, 0)
         GPIO.output(self.ML_B, 1)
 
-    # otočení na místě směrem doleva
+    # turn left (on spot)
     def left(self):
         GPIO.output(self.MR_F, 1)
         GPIO.output(self.MR_B, 0)
         GPIO.output(self.ML_F, 0)
         GPIO.output(self.ML_B, 1)
 
-    # otočení na místě směrem doprava
+    # turn right (on spot)
     def right(self):
         GPIO.output(self.MR_F, 0)
         GPIO.output(self.MR_B, 1)
         GPIO.output(self.ML_F, 1)
         GPIO.output(self.ML_B, 0)
 
-    # zastavení
+    # brake
     def brake(self):
         GPIO.output(self.MR_F, 0)
         GPIO.output(self.MR_B, 0)
         GPIO.output(self.ML_F, 0)
         GPIO.output(self.ML_B, 0)
 
-    # aktivace motorů
+    # activate motors
     def start(self):
         self.power_MR.start(self.SPEED)
         self.power_ML.start(self.SPEED)
 
-    # deaktivace motorů
+    # deactivate motors
     def stop(self):
         self.power_MR.stop()
         self.power_ML.stop()
 
-    # změna rychlosti otáčení motorů pro oba motory
+    # change the rotation speed of the motors for both motors
     def change_speed(self, new_speed):
         self.power_MR.ChangeDutyCycle(new_speed)
         self.power_ML.ChangeDutyCycle(new_speed)
 
-    # změna rychlosti otáčení peo pravý motor
+    # change the rotation speed of the motors for right motor
     def change_speed_right(self, new_speed):
         self.power_MR.ChangeDutyCycle(new_speed)
 
-    # změna rychlosti otáčení pro levý motor
+    # change the rotation speed of the motors for left motor
     def change_speed_left(self, new_speed):
         self.power_ML.ChangeDutyCycle(new_speed)
 
-    # zatočení doleva během jízdy vpřed
+    # turn left
     def turn_left(self):
         GPIO.output(self.MR_F, 1)
         GPIO.output(self.MR_B, 0)
         GPIO.output(self.ML_F, 0)
         GPIO.output(self.ML_B, 0)
 
-    # zatočení doprava během jízdy vpřed
+    # turn right 
     def turn_right(self):
         GPIO.output(self.MR_F, 0)
         GPIO.output(self.MR_B, 0)
         GPIO.output(self.ML_F, 1)
         GPIO.output(self.ML_B, 0)
 
-    # zatočení doprava během jízdy vzad
+    # turn right while moving backward
     def turn_right_back(self):
         GPIO.output(self.MR_F, 0)
         GPIO.output(self.MR_B, 0)
         GPIO.output(self.ML_F, 0)
         GPIO.output(self.ML_B, 1)
 
-   # zatočení doleva během jízdy vzad
+   # turn left while moving backward
     def turn_left_back(self):
         GPIO.output(self.MR_F, 0)
         GPIO.output(self.MR_B, 1)
